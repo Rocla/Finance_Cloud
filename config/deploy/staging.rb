f90b5b5fe7aa3f5e5dabb736644b39bda44b7aa5
Romain Claret <contact@rocla.ch>
2016-02-17 09:36:29 +0100

    / staging server configuration

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server 'srvz-webapp.he-arc.ch', user: 'romain', roles: %w{app db web}
+#server 'srvz-webapp.he-arc.ch', user: 'matthieu', roles: %w{app db web}
+#server 'srvz-webapp.he-arc.ch', user: 'fabio', roles: %w{app db web}
 
 
 # role-based syntax
