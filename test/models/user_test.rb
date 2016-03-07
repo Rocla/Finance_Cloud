56bd5c879c6915c6aca4013886eb8ee5081ff34f
matthieu.bandelie <matbandeli@gmail.com>
2016-03-07 16:52:02 +0100

    unit test - user/password update

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,40 +6,48 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "can register" do
-	user = User.new(username: "UserX", email: "lol@finance.cloud", password: "admin")
+	user = User.new(username: "UserX", email: "lol@finance.cloud", password: "admin2")
     assert user.save, "Error : fail saving valid user"
   end
 
   test "no username twice" do
-	user = User.new(username: $user.username, email: "lol@finance.cloud", password: "admin")
+	user = User.new(username: $user.username, email: "lol@finance.cloud", password: "admin2")
     assert !user.save, "Error : success saving user with same username"
   end
   
   test "no email twice" do
-	user = User.new(username: "userX", email: $user.email, password: "admin")
+	user = User.new(username: "userX", email: $user.email, password: "admin2")
     assert !user.save, "Error : success saving user with same email"
   end
   
   test "username too short" do
-    user = User.new(username: "Ad", email: "lol@finance.cloud", password: "admin")
+    user = User.new(username: "Ad", email: "lol@finance.cloud", password: "admin2")
     assert !user.save, "Error : sucess saving user, username too short"
   end
   
   test "username too long" do
-    user = User.new(username: "1234567890123456", email: "lol@finance.cloud", password: "admin")
+    user = User.new(username: "1234567890123456", email: "lol@finance.cloud", password: "admin2")
     assert !user.save, "Error : sucess saving user, username too long"
   end
   
   test "invalid email" do
-    user = User.new(username: "Admin2", email: "lol@finance.", password: "admin")
+    user = User.new(username: "Admin2", email: "lol@finance.", password: "admin2")
     assert !user.save, "Error : sucess saving user, invalid email"
   end
   
   test "invalid rank" do
-    user = User.new(username: "Admin3", email: "lol@finance.cloud", password: "admin", rank: 1000)
+    user = User.new(username: "Admin3", email: "lol@finance.cloud", password: "admin2", rank: 1000)
     assert !user.save, "Error : sucess saving user, invalid rank"
   end
   
-  #TODO password tests
+  test "password too short" do
+    user = User.new(username: "UserX", email: "lol@finance.cloud", password: "admin")
+    assert !user.save, "Error : sucess saving user, username too short"
+  end
+  
+  test "password too long" do
+    user = User.new(username: "UserX", email: "lol@finance.cloud", password: "123456789012345678901234567890123456789012345678901")
+    assert !user.save, "Error : sucess saving user, username too long"
+  end
   
 end
