8d15b09d852bbd9cba6cf8854465d7bb5db691c1
Romain Claret <contact@rocla.ch>
2016-03-14 13:39:50 +0100

    / elegant js search

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,12 +1,9 @@
 var init_stock = function()
 {
-  $('#stock-search-id').on('ajax:success', function(event, data, status)
+  $('#stock-search').on('ajax:success', function(event, data)
   {
-    $('#stock-search').replaceWith(data);
-    init_stock();
-  });
-
-  $('#stock-search-id').on('ajax:error', function(event, xhr, status, error)
+    $(this).html(data);
+  }).on('ajax:error', function()
   {
     $('#stock-search-error').replaceWith('Stock not found');
   });
