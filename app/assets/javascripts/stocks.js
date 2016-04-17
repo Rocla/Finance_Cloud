f58d42ba00b7a24101b27417ff59009626bc92fd
Romain Claret <contact@rocla.ch>
2016-04-17 13:19:57 +0200

    + spinner during research of stocks

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,10 +1,19 @@
 var init_stock = function()
 {
+  $('#stock-search').on('ajax:before', function(event, data)
+  {
+    show_spinner();
+  });
+  $('#stock-search').on('ajax:after', function(event, data)
+  {
+    hide_spinner();
+  });
   $('#stock-search').on('ajax:success', function(event, data)
   {
     $(this).html(data);
   }).on('ajax:error', function()
   {
+    hide_spinner();
     $('#stock-search-error').replaceWith('Stock not found');
   });
 }
